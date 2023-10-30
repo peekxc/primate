979c19d6c97469ca135877ae93afa72b7eebc704
peekxc <matt.piekenbrock@gmail.com>
2023-10-30 16:15:18 -0400

    Add experimental xtrace estimator, needs to be incorporated via e.g. pythran, mypyc, or something once the lanczos matvec estimator is done

---
+++ examples/lanczos.py
@@ -0,0 +1,111 @@
+import numpy as np 
+
+# %% 
+np.random.seed(1234)
+ew = np.random.uniform(size=15, low=0, high=2)
+Q,R = np.linalg.qr(np.random.uniform(size=(15,15)))
+A = Q @ np.diag(ew) @ Q.T
+A = (A + A.T) / 2
+assert np.all(A == A.T)
+# from numpy.polynomial.polynomial import polyfit
+# polyfit()
+
+
+# %% 
+from primate.diagonalize import lanczos
+from scipy.linalg import eigh_tridiagonal
+np.random.seed(1234)
+v0 = np.array(np.random.uniform(size=A.shape[0]))
+a,b = lanczos(A, v0, max_steps=10)
+
+def lanczos_action(z: np.ndarray, A, alpha, beta, v: np.ndarray):
+  """Yields the matrix y = Qz where T(alpha, beta) = Q^T A Q is the tridiagonal matrix spanning K(A, v)"""
+  n, k = A.shape[0], len(z)
+  assert len(v) == n, "v dimension mismatch"
+  y = np.zeros(n) # output vector
+  av, bv = np.append([0], alpha), np.append([0,0], beta) 
+  qp, qc = np.zeros(n), v / np.linalg.norm(v) # previous, current
+  for i in range(1,k+1):
+    qn = A @ qc - bv[i]*qp - av[i]*qc 
+    y += z[i-1]*qc 
+    qp, qc = qc, qn 
+  return y
+
+from typing import *
+def matrix_function_lanczos(A, f: Callable, v: np.ndarray, k: int = 15):
+  """Approximates the action v -> f(A)v via the Lanczos method"""
+  assert len(v) == A.shape[0], "Dimension mismatch"
+  k = min(k, A.shape[0])
+  a,b = lanczos(A, v, max_steps=k)
+  rr, V = eigh_tridiagonal(a,b[:-1])
+  z = V @ (f(rr) * V[0,:])  ## Compute < f(T), e_1 >
+  return np.linalg.norm(v)*lanczos_action(z, A, a, b, v)
+
+identity = lambda x: x
+truth_Av = A @ v0
+
+matrix_function_lanczos(A, identity, v0, A.shape[0])
+
+normalize = lambda x: x / np.linalg.norm(x)
+
+
+from primate.random import rademacher
+samples, samples_mean = [], []
+for i in range(300):
+  rv = rademacher(A.shape[0])
+  rv_f = matrix_function_lanczos(A, identity, rv, A.shape[0])
+  samples.append(rv_f)
+  samples_mean.append(np.dot(rv, rv_f))
+
+mean_trace = np.cumsum(samples_mean)/np.arange(1,301)
+
+np.dot(normalize(A @ v0), normalize(v0)) # 
+
+## Truth 
+a,b = lanczos(A, v0, max_steps=A.shape[0], sparse_mat=False)
+T = lanczos(A, v0, max_steps=A.shape[0], sparse_mat=True)
+y = np.linalg.norm(v0) * lanczos_action(T @ np.array([1] + [0]*(T.shape[1]-1)), A, a, b, v0)
+
+np.dot(normalize(y), normalize(A @ v0))
+
+## Can do block-wise computation
+eigh_tridiagonal(a,b[:-1], select='i', select_range=(0,0))
+
+
+
+
+
+
+
+## XTRACE on (n x n) matrix A
+import numpy as np 
+from scipy.linalg import solve_triangular
+from scipy.sparse.linalg import spsolve_triangular
+from scipy.sparse import spdiags, diags
+# spsolve_triangular(R, diags(np.ones(m)))
+
+np.trace(A) ## the goal
+
+xtrace(A, 30, method='rademacher')
+
+xtrace(A, 10, method='sphere')
+xtrace(A, 10, method='improved')
+
+## NOTE: Xtrace doesn't do this; see xtrace_tol
+# samples_xtrace = np.ravel([xtrace(A, 30, method='rademacher')[0] for i in range(100)])
+# samples_xtrace = np.cumsum(samples_xtrace) / np.arange(1, len(samples_xtrace)+1)
+show(figure_trace(samples_xtrace))
+
+tr_est, info = slq(A, max_num_samples=100, return_info=True)
+
+show(figure_trace(np.ravel(info['convergence']['samples'])))
+
+
+
+
+
+
+# %% 
+import numpy as np 
+
+x = np.random.uniform(size=10)
---
+++ examples/xtrace.py
@@ -0,0 +1,106 @@
+# %%
+import numpy as np
+from scipy.io import loadmat
+
+np.random.seed(1234)
+ew = np.random.uniform(size=150, low=0, high=2)
+Q, R = np.linalg.qr(np.random.uniform(size=(len(ew), len(ew))))
+A = Q @ np.diag(ew) @ Q.T
+A = (A + A.T) / 2
+assert np.all(A == A.T)
+
+# X = loadmat("/Users/mpiekenbrock/XTrace/X.mat")["X"]
+# Om = loadmat("/Users/mpiekenbrock/XTrace/Om.mat")["Om"]
+# print(X.trace())
+# xtrace(X)
+
+# Y = X @ Om
+# Q, R = np.linalg.qr(Y, "reduced")
+# Z = A @ Q
+
+# sample_isotropic(8, 1, method="improved")
+
+
+# %% 
+def sample_isotropic(n: int, m: int, method: str = "rademacher"):
+	from primate.random import rademacher
+	if method == "rademacher":
+		return rademacher(size=(n, m))
+	elif method == "improved":
+		W = np.random.normal(size=(n, m))
+		W = np.sqrt(n) * (W / np.linalg.norm(W, axis=0))  ## confirmed
+		return W
+	elif method == "sphere":
+		W = np.random.normal(size=(n, m))
+		W = np.sqrt(n) * (W @ np.diag(1.0 / np.linalg.norm(W, axis=0)))
+		return W
+	else:
+		raise ValueError("invalid method supplied")
+
+def __xtrace(W: np.ndarray, Z: np.ndarray, Q: np.ndarray, R: np.ndarray, method: str):
+	"""Helper for xtrace function"""
+	from scipy.linalg import solve_triangular
+	diag_prod = lambda A, B: np.diag(A.T @ B)[:, np.newaxis]
+
+	## Invert R
+	n, m = W.shape
+	W_proj = Q.T @ W
+	R_inv = solve_triangular(R, np.identity(m)).T
+	S = (R_inv / np.linalg.norm(R_inv, axis=0)) # this is cnormc
+
+	## Handle the scale
+	if not method == 'improved':
+	  scale = np.ones(m)[:,np.newaxis] # this is a column vector
+	else:
+	  ## This ends up being a square matrix
+	  col_norm = lambda X: np.linalg.norm(X, axis=0)
+	  c = n - m + 1
+	  scale = c / (n - (col_norm(W_proj)[:,np.newaxis])**2 + (diag_prod(S,W_proj) * col_norm(S)[:,np.newaxis])**2)
+
+	## Intermediate quantities
+	H = Q.T @ Z
+	HW = H @ W_proj
+	T = Z.T @ W
+	dSW, dSHS = diag_prod(S, W_proj), diag_prod(S, H @ S)
+	dTW, dWHW = diag_prod(T, W_proj), diag_prod(W_proj, HW)
+	dSRmHW, dTmHRS = diag_prod(S, R - HW), diag_prod(T - H.T @ W_proj, S)
+
+	## Trace estimate
+	tr_ests = np.trace(H) * np.ones(shape=(m, 1)) - dSHS
+	tr_ests += (-dTW + dWHW + dSW * dSRmHW + abs(dSW) ** 2 * dSHS + dTmHRS * dSW) * scale
+	t = tr_ests.mean()
+	err = np.std(tr_ests) / np.sqrt(m)
+	return t, tr_ests, err
+
+
+def xtrace(A, error_atol: float = 0.1, error_rtol: float = 1e-6, nv: int = 1, cond_tol: float = 1e8, method: str = "improved"):
+	assert error_atol >= 0.0 and error_rtol >= 0.0, "Error tolerances must be positive"
+	assert cond_tol >= 0.0, "Condition number must be non-negative"
+	n = A.shape[0]
+	Y, Om, Z = np.zeros(shape=(n, 0)), np.zeros(shape=(n, 0)), np.zeros(shape=(n, 0))
+	t, err = np.inf if (error_rtol != 0) else 0, np.inf
+	it = 0
+	while Y.shape[1] < A.shape[1]:  # err >= (error_atol + error_rtol * abs(t)):
+		ns = max(nv,2) if it == 0 else nv 
+		ns = min(A.shape[1] - Y.shape[1], ns)
+		NewOm = sample_isotropic(n, ns, method=method)
+		
+		## If the sampled vectors have a lot of linear dependence  won't be (numerically) span a large enough subspace
+		## to permit sufficient exploration of the eigen-space, so we optionally re-sample based on a loose upper bound
+		## Based on: https://math.stackexchange.com/questions/1191503/conditioning-of-triangular-matrices
+		cond_numb_bound = np.inf
+		while cond_numb_bound > cond_tol:
+			tmp_Y, tmp_Om = np.c_[Y, A @ NewOm], np.c_[Om, NewOm]
+			Q, R = np.linalg.qr(tmp_Y, "reduced") 
+			R_mass = np.abs(np.diag(R))
+			_cn = 3 * np.max(R_mass) / np.min(R_mass)
+			cond_numb_bound = 0.0 if np.isclose(_cn, cond_numb_bound) else _cn
+			# print(cond_numb_bound)
+		Y, Om = tmp_Y, tmp_Om 
+		Z = np.c_[Z, A @ Q[:, -ns:]]
+		t, t_samples, err = __xtrace(Om, Z, Q, R, method)
+		it += 1
+		print(t)
+	return t, t_samples, err
+
+savemat("/Users/mpiekenbrock/XTrace/bad_Om.mat", { 'Om' : NewOm })
