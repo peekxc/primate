8ad949002b88fed60f6cf3ec97cbc70517411622
peekxc <matt.piekenbrock@gmail.com>
2023-12-05 12:31:51 -0500

    Adding pypy, removing musl, fixing windows repair command

--- tools/cibw_linux.sh
+++ tools/cibw_linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -n "$(command -v yum)" ]; then 
-  dng check-update
+  dnf check-update
   dnf update
   yum install llvm-toolset
   yum install openblas
@@ -14,4 +14,13 @@ elif [ -n "$(command -v apt-get)" ]; then
   apt-get update -y
   apt-get install -y libopenblas-dev
   apt-get install -y python3-dev
+elif [ -n "$(command -v apk)" ]; then 
+  apk update
+  apk add clang # looks like Alpine only supports up to clang 10 ?
+  # apk add openmp
+  apk add openblas
+  apk add python3-dev
+  alias python=python3
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python get-pip.py
 fi 
