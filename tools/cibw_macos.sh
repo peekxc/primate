386d705922c2e597cfe1668d1193ab4b916213b8
peekxc <matt.piekenbrock@gmail.com>
2023-12-05 08:40:28 -0500

    fresh changes for the CI

--- tools/cibw_macos.sh
+++ tools/cibw_macos.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-brew install --force libomp llvm openblas
 rm -rf /usr/local/bin/2to3/*
+brew install --force libomp llvm openblas
 
 export CC=/usr/local/opt/llvm/bin/clang
 export CXX=/usr/local/opt/llvm/bin/clang++
 export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-export CFLAGS="$CFLAGS -I$PREFIX/include"
+export CFLAGS="$CFLAGS -Wno-implicit-function-declaration -I$PREFIX/include"
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
+export LDFLAGS="$LDFLAGS -Wl,-S -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
 
 if [[ $(uname -m) == "arm64" && "$CIBW_BUILD" == "cp38-macosx_arm64" ]]; then
   # Enables native building and testing for macosx arm on Python 3.8. For details see:
@@ -18,11 +19,40 @@ if [[ $(uname -m) == "arm64" && "$CIBW_BUILD" == "cp38-macosx_arm64" ]]; then
   sh "/Applications/Python 3.8/Install Certificates.command"
 fi
 
-echo CXX VARIABLE: 
-echo $CXX
-rm /usr/bin/clang
-rm /usr/bin/clang++
-ln -s $CC /usr/bin/clang
-ln -s $CXX /usr/bin/clang++
-alias clang=/usr/local/opt/llvm/bin/clang
-alias clang++=/usr/local/opt/llvm/bin/clang++
+echo CXX VARIABLE: $CXX
+clang --version
+
+
+#  # Make sure to use a libomp version binary compatible with the oldest
+#   # supported version of the macos SDK as libomp will be vendored into
+#   # the scikit-image wheels for macos. The list of binaries are in
+#   # https://packages.macports.org/libomp/.  Currently, the oldest
+#   # supported macos version is: High Sierra / 10.13. When upgrading
+#   # this, be sure to update the MACOSX_DEPLOYMENT_TARGET environment
+#   # variable accordingly. Note that Darwin_17 == High Sierra / 10.13.
+#   #
+#   # We need to set both MACOS_DEPLOYMENT_TARGET and MACOSX_DEPLOYMENT_TARGET
+#   # until there is a new release with this commit:
+#   # https://github.com/mesonbuild/meson-python/pull/309
+#   if [[ "$CIBW_ARCHS_MACOS" == arm64 ]]; then
+#       # SciPy requires 12.0 on arm to prevent kernel panics
+#       # https://github.com/scipy/scipy/issues/14688
+#       # so being conservative, we just do the same here
+#       export MACOSX_DEPLOYMENT_TARGET=12.0
+#       export MACOS_DEPLOYMENT_TARGET=12.0
+#       OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
+#   else
+#       export MACOSX_DEPLOYMENT_TARGET=10.9
+#       export MACOS_DEPLOYMENT_TARGET=10.9
+#       OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+#   fi
+#   echo MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}
+#   echo MACOS_DEPLOYMENT_TARGET=${MACOS_DEPLOYMENT_TARGET}
+
+#   # use conda to install llvm-openmp
+#   # Note that we do NOT activate the conda environment, we just add the
+#   # library install path to CFLAGS/CXXFLAGS/LDFLAGS below.
+#   sudo conda create -n build $OPENMP_URL
+#   PREFIX="/usr/local/miniconda/envs/build"
+#   export CC=/usr/bin/clang
+#   export CXX=/usr/bin/clang++
