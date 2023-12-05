5d55852bdc80d15d8435d6d1d697dcb9ceeee865
peekxc <matt.piekenbrock@gmail.com>
2023-12-05 11:24:56 -0500

    More tweaks for the build

--- tools/cibw_windows.sh
+++ tools/cibw_windows.sh
@@ -5,18 +5,18 @@
 # https://github.com/actions/virtual-environments/issues/294#issuecomment-588090582
 # with additional -arch=x86 flag to vsdevcmd.bat
 # https://github.com/actions/runner-images/issues/294
-function Invoke-VSDevEnvironment {
-  $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    $installationPath = & $vswhere -prerelease -legacy -latest -property installationPath
-    $Command = Join-Path $installationPath "Common7\Tools\vsdevcmd.bat"
-  & "${env:COMSPEC}" /s /c "`"$Command`" -arch=amd64 -no_logo && set" | Foreach-Object {
-        if ($_ -match '^([^=]+)=(.*)') {
-            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
-        }
-    }
-}
-Invoke-VSDevEnvironment
-Get-Command rc.exe | Format-Table -AutoSize
+# function Invoke-VSDevEnvironment {
+#   $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+#     $installationPath = & $vswhere -prerelease -legacy -latest -property installationPath
+#     $Command = Join-Path $installationPath "Common7\Tools\vsdevcmd.bat"
+#   & "${env:COMSPEC}" /s /c "`"$Command`" -arch=amd64 -no_logo && set" | Foreach-Object {
+#         if ($_ -match '^([^=]+)=(.*)') {
+#             [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+#         }
+#     }
+# }
+# Invoke-VSDevEnvironment
+# Get-Command rc.exe | Format-Table -AutoSize
 
 choco install rtools -y --no-progress --force --version=4.0.0.20220206"
 set PATH=C:\rtools40\ucrt64\bin\;%PATH%
