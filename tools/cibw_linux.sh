dcf8f5431be09cb0884b3bbcccb2d5582ee56d37
peekxc <matt.piekenbrock@gmail.com>
2023-12-05 13:45:59 -0500

    Adding simple pip-based wheel build instead of complicated delvewheel

--- tools/cibw_linux.sh
+++ tools/cibw_linux.sh
@@ -9,11 +9,11 @@ if [ -n "$(command -v yum)" ]; then
   yum install python39-devel
   alias python=python3.9
 elif [ -n "$(command -v apt-get)" ]; then 
-  apt-get update -y
-  apt-get install -y clang libomp-dev
-  apt-get update -y
-  apt-get install -y libopenblas-dev
-  apt-get install -y python3-dev
+  sudo apt-get update -y
+  sudo apt-get install -y clang libomp-dev
+  sudo apt-get update -y
+  sudo apt-get install -y libopenblas-dev
+  sudo apt-get install -y python3-dev
 elif [ -n "$(command -v apk)" ]; then 
   apk update
   apk add clang # looks like Alpine only supports up to clang 10 ?
