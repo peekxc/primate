49b5cb126d3d85fd96041ed0d4709917f6bfadc0
peekxc <matt.piekenbrock@gmail.com>
2023-12-07 13:16:23 -0500

    windows repair changes

--- tools/repair_windows.sh
+++ tools/repair_windows.sh
@@ -1,26 +1,25 @@
 set -xe
 
-WHEEL="$1"
-DEST_DIR="$2"
+WHEELDIR=$GITHUB_WORKSPACE/wheelhouse
+WHEELNAME=$(find "$WHEELDIR" -type f | grep *primate*.whl)
 
-strip .local/bin/*.dll
+# strip .local/bin/*.dll
 
 # Make sure to leave the wheel in the same directory
-wheeldir=$(dirname $WHEELNAME)
-pushd $wheeldir
-  # Unpack the wheel and strip any .pyd DLLs inside
-  wheel unpack $WHEELNAME
-  rm $WHEELNAME
-  strip python_flint-*/flint/*.pyd
-  wheel pack python_flint-*
-popd
+# wheeldir=$(dirname $WHEELNAME)
+# pushd $wheeldir
+#   # Unpack the wheel and strip any .pyd DLLs inside
+#   wheel unpack $WHEELNAME
+#   rm $WHEELNAME
+#   strip python_flint-*/flint/*.pyd
+#   wheel pack python_flint-*
+# popd
 
 # Make the wheel relocatable. This will fail with an error message about
 # --no-mangle if strip has not been applied to all mingw64-created .dll and
 # .pyd files that are needed for the wheel.
 delvewheel repair $WHEELNAME 	\
-	-w $WHEELHOUSE		\
-	--add-path .local/bin
+	--wheel-dir $WHEELHOUSE
 
 # ## Need delvewheel for this 
 # pip install delvewheel
