cfc149f430429f8423a84a3b01c36a4ab2fb287c
peekxc <matt.piekenbrock@gmail.com>
2023-12-08 10:37:20 -0500

    Cleaning up by keeping the old content indexed but otherwise only local

--- todo/run_parallel.py
+++
@@ -1,7 +0,0 @@
-from timeit import timeit
-import numpy as np
-from pyimate import _random_generator
-z = np.zeros(150000, np.float32)
-
-timeit(lambda: _random_generator.rademacher_xs(z, 1), number=1000)
-timeit(lambda: _random_generator.rademacher_xs(z, 2), number=1000)
